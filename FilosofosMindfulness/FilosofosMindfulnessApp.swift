634cdd76ec0c01617bada83ccdc0f3d071fdf971
Cristian Ramirez <Crf.21@iCloud.Com>
2023-12-05 17:17:35 -0500

    Initial Commit

---
+++ FilosofosMindfulness/FilosofosMindfulnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilosofosMindfulnessApp.swift
+//  FilosofosMindfulness
+//
+//  Created by Cristian Ramirez on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FilosofosMindfulnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
